0a8d389c95d79dfc5b7cf57f986c70529bbb7bd0
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-05-26 20:11:13 +0200

    Updated requirements.txt

--- server/Server.py
+++ server/Server.py
@@ -183,7 +183,8 @@ class Server:
 
             self.mouse_listener.start()
             time.sleep(0.5)
-            #self.keyboard_listener.start()
+            self.log("Starting keyboard listener.")
+            self.keyboard_listener.start()
 
             if not self.mouse_listener.is_alive():
                 raise Exception("Mouse listener not started")
