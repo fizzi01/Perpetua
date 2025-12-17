ddf1425d810802d1220843625bd668748821dc52
fizzi01 <federico.izzi@studenti.unisalento.it>
2025-12-17 01:16:45 +0100

    refactor: add movement history thresholds redefinition for windows

--- input/mouse/_win.py
+++ input/mouse/_win.py
@@ -8,6 +8,10 @@ class ServerMouseListener(_base.ServerMouseListener):
     It listens for mouse events on Windows systems.
     Its main purpose is to capture mouse movements and clicks. And handle some border cases like cursor reaching screen edges.
     """
+
+    MOVEMENT_HISTORY_N_THRESHOLD = 4
+    MOVEMENT_HISTORY_LEN = 5
+
     def _win32_mouse_suppress_filter(self, msg, data):
         """
         Suppress mouse events when listening.
