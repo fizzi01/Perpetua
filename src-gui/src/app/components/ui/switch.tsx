85f027f7f4e0ec69f7d641d97c0fd35ff7717358
fizzi01 <federico.izzi@studenti.unisalento.it>
2026-01-27 00:36:17 +0100

    feat(gui): remove unnecessary htmlFor attributes, change disabled switch cursor

--- src-gui/src/app/components/ui/switch.tsx
+++ src-gui/src/app/components/ui/switch.tsx
@@ -18,7 +18,7 @@ const Switch = React.forwardRef<
   return (
     <SwitchPrimitives.Root
       className={cn(
-        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 shadow-sm transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 shadow-sm transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50",
         className
       )}
       style={{
