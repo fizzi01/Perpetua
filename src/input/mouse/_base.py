819e474733a5760f9f68b7c4b7c7ebe1a2161abe
Fed1z <105501091+fizzi01@users.noreply.github.com>
2026-02-07 13:56:55 +0100

    fix(mouse): reduce double-click detection time from 200ms to 100ms

--- src/input/mouse/_base.py
+++ src/input/mouse/_base.py
@@ -1023,8 +1023,8 @@ class ClientMouseController(object):
             self._controller.release(btn)
             self._pressed = False
         elif not self._pressed and is_pressed:
-            # If we receive a press event within 200ms of the last press, treat it as a double-click
-            if (current_time - self._last_press_time) < 0.2:
+            # If we receive a press event within 100ms of the last press, treat it as a double-click
+            if (current_time - self._last_press_time) < 0.1:
                 self._controller.click(btn, 2 + self._doubleclick_counter)
                 self._doubleclick_counter = 0 if self._doubleclick_counter == 2 else 2
                 self._pressed = False
