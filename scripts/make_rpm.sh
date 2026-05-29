5a7127d97d69562ab8870fe007a55e3e30d410db
fizzi01 <federico.izzi@studenti.unisalento.it>
2026-05-29 10:00:28 +0200

    ci(build): update dependencies for Debian and RPM packaging

--- scripts/make_rpm.sh
+++ scripts/make_rpm.sh
@@ -161,6 +161,13 @@ BuildArch:      ${RPM_ARCH}
 
 AutoReqProv:    no
 Requires:       libei
+Requires:       webkit2gtk4.1
+Requires:       gtk3
+Requires:       libsoup3
+Requires:       cairo
+Requires:       glib2
+Requires:       gdk-pixbuf2
+Requires:       libayatana-appindicator-gtk3
 Recommends:     xclip
 Recommends:     wl-clipboard
 Recommends:     liboeffis
